085b059284f23844743ae1cfabdfaa30be974196
Ke Li <kel4@andrew.cmu.edu>
2024-03-26 19:22:52 -0700

    edit run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 ###### TEMPLATE run.sh ######
 ###### YOU NEED TO UNCOMMENT THE FOLLOWING LINE AND INSERT YOUR OWN PARAMETERS ######
 
-spark-submit --conf spark.scheduler.mode=FAIR	--conf spark.dynamicAllocation.enabled=false --conf spark.sql.adaptive.enabled=true --conf spark.sql.autoBroadcastJoinThreshold=20 --conf spark.driver.memory=25g --conf spark.executor.cores=4 --conf spark.default.parallelism=40 --num-executors=10	--executor-memory 5g --class PageRank target/project_spark.jar wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph-Topics wasbs:///pagerank-output wasbs:///recs-output
+spark-submit --conf spark.scheduler.mode=FAIR	--conf spark.dynamicAllocation.enabled=false --conf spark.sql.adaptive.enabled=true --conf spark.sql.autoBroadcastJoinThreshold=20 --conf spark.driver.memory=25g --conf spark.executor.cores=4 --conf spark.default.parallelism=40 --num-executors=10	--executor-memory 15g --class PageRank target/project_spark.jar wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph-Topics wasbs:///pagerank-output wasbs:///recs-output
 
 # For more information about tuning the Spark configurations, refer: https://spark.apache.org/docs/2.3.0/configuration.html

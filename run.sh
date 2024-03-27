a72096bb5b0ccd87512aae6849b973a92d896934
Ke Li <kel4@andrew.cmu.edu>
2024-03-26 20:18:17 -0700

    edit run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 ###### TEMPLATE run.sh ######
 ###### YOU NEED TO UNCOMMENT THE FOLLOWING LINE AND INSERT YOUR OWN PARAMETERS ######
 
-spark-submit	--conf spark.dynamicAllocation.enabled=true --conf spark.sql.adaptive.enabled=true --conf spark.sql.autoBroadcastJoinThreshold=200 --conf spark.driver.memory=8g --conf spark.executor.cores=7 --conf spark.default.parallelism=20 --num-executors=20	--executor-memory 10g --class PageRank target/project_spark.jar wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph-Topics wasbs:///pagerank-output wasbs:///recs-output
+spark-submit -conf spark.sql.adaptive.enabled=true --conf spark.sql.autoBroadcastJoinThreshold=200 --conf spark.driver.memory=8g --conf spark.executor.cores=1 --conf spark.default.parallelism=20 --num-executors=20	--executor-memory 6g --class PageRank target/project_spark.jar wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph-Topics wasbs:///pagerank-output wasbs:///recs-output
 
 # For more information about tuning the Spark configurations, refer: https://spark.apache.org/docs/2.3.0/configuration.html
